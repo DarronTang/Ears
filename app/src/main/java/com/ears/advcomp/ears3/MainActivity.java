e65cf699c5f782f4da108dc5652c14125504ef29
Zhong-yuen Lee <zl21g13@soton.ac.uk>
2017-06-01 17:39:59 +0100

    To delete. Please ignore this commit.

--- app/src/main/java/com/ears/advcomp/ears3/MainActivity.java
+++ app/src/main/java/com/ears/advcomp/ears3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ears.advcomp.ears3;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -9,24 +10,32 @@ import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static final String LOG_TAG = "Darron";
+    static final String REL_DIR = "DemEars";
+    static final String EAR_CSV = "/ears.csv";
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -43,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermissions();
+        checkStorageAvailabilty();
 
         addEar = (Button)findViewById(R.id.addEar);
         findEar = (Button)findViewById(R.id.findEar);
@@ -56,35 +66,32 @@ public class MainActivity extends AppCompatActivity {
         findEar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent findEarIntent = new Intent(getApplicationContext(),FindEarActivity.class);
+                Intent findEarIntent = new Intent(getApplicationContext(), FindEarActivity.class);
                 startActivity(findEarIntent);
             }
         });
 
         try {
             File storageDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
-            earCSV = new File(storageDir.getAbsolutePath() + "/ears.csv");
+            try {
+                earCSV = new File(storageDir.getAbsolutePath() + EAR_CSV);
+            } catch (NullPointerException e) {
+                Log.e(LOG_TAG, "Absolute path for Documents directory not found");
+            }
             if(!earCSV.exists()) {
                 earCSV.createNewFile();
-
-
                 try {
                     PrintWriter out = new PrintWriter(earCSV);
                     out.write("first_name,last_name,inv_moment\n");
                     out.close();
                 } catch (FileNotFoundException e) {
-                    Log.d("Darron", "File not found: " + e.getMessage());
-                } catch (IOException e) {
-                    Log.d("Darron", "Error accessing file: " + e.getMessage());
+                    Log.d(LOG_TAG, "File not found: " + e.getMessage());
                 }
-
             }
         } catch (IOException e){
-            Log.e("Darron", "onCreate: "+e.getMessage());
+            Log.e(LOG_TAG, "Error accessing file in onCreate(): " + e.getMessage());
         }
     }
-
-    static final int REQUEST_TAKE_PHOTO = 1;
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -94,18 +101,30 @@ public class MainActivity extends AppCompatActivity {
             File photoFile = null;
             try {
                 photoFile = createImageFile();
-            } catch (IOException ex) {
+            } catch (IOException e) {
                 // Error occurred while creating the File
-                Log.e("Darron", "dispatchTakePictureIntent: " + ex.getMessage());
+                Log.e(LOG_TAG, "dispatchTakePictureIntent: " + e.getMessage());
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
+        }
+    }
+
+    private void checkStorageAvailabilty() {
+        if (!isExternalStorageReadable() || !isExternalStorageWritable()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+            builder.setTitle(R.string.no_ext_storage_title)
+                    .setMessage(R.string.no_ext_storage_body)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            finish();
+                        }
+                    }).create().show();
         }
     }
 
@@ -113,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = getAlbumStorageDir(REL_DIR);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -138,9 +157,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
+    /* Checks if external storage is available for read and write */
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    private boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    private File getAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory
+                (Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e(LOG_TAG, "Directory not created");
+        }
+        return file;
+    }
+
+    /*
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
 //    public native String stringFromJNI();
+
 }
